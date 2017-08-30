965fabfef5437dc36de53f23c74b6305dedfb82d
Furikake <ewave6300@gmail.jp>
2017-08-30 16:50:50 +0900

    Vector2Int型にVector2へのキャストを追加

--- Assets/Scripts/Vector2Int.cs
+++ Assets/Scripts/Vector2Int.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using UnityEngine;
 
+[Serializable]
 class Vector2Int
 {
     public int x { get; set; }
     public int y { get; set; }
 
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="x">整数X</param>
+    /// <param name="y">整数Y</param>
     public Vector2Int(int x, int y)
     {
         this.x = x;
         this.y = y;
+    }
+
+    /// <summary>
+    /// Vector2型へのキャスト
+    /// </summary>
+    /// <param name="value">Vector2型の値</param>
+    public static implicit operator Vector2(Vector2Int value)
+    {
+        return new Vector2((float)value.x, (float)value.y);
     }
 }
