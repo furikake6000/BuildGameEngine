84ca9590a62b82888277892f840ec766e91c4403
Furikake <ewave6300@gmail.jp>
2017-08-30 19:32:39 +0900

    Vector2の演算子定義

--- Assets/Scripts/Vector2Int.cs
+++ Assets/Scripts/Vector2Int.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 
 [Serializable]
-class Vector2Int
+public class Vector2Int
 {
     public int x { get; set; }
     public int y { get; set; }
@@ -26,4 +26,54 @@ class Vector2Int
     {
         return new Vector2((float)value.x, (float)value.y);
     }
+
+    #region 演算子定義
+    public static Vector2Int operator +(Vector2Int v1, Vector2Int v2)
+    {
+        return new Vector2Int(v1.x + v2.x, v1.y + v2.y);
+    }
+
+    public static Vector2Int operator -(Vector2Int v1, Vector2Int v2)
+    {
+        return new Vector2Int(v1.x - v2.x, v1.y - v2.y);
+    }
+
+    public static Vector2Int operator *(Vector2Int v1, int i1)
+    {
+        return new Vector2Int(v1.x * i1, v1.y * i1);
+    }
+    public static Vector2Int operator *(int i1, Vector2Int v1)
+    {
+        return v1 * i1;
+    }
+
+    public static Vector2Int operator *(Vector2Int v1, float f1)
+    {
+        return new Vector2Int((int)(f1 * v1.x), (int)(f1 * v1.y));
+    }
+    public static Vector2Int operator *(float f1, Vector2Int v1)
+    {
+        return v1 * f1;
+    }
+
+    public static bool operator ==(Vector2Int v1, Vector2Int v2)
+    {
+        //nullの確認
+        if (object.ReferenceEquals(v1, v2))
+        {
+            return true;
+        }
+        if (((object)v1 == null) || ((object)v2 == null))
+        {
+            return false;
+        }
+
+        return (v1.x == v2.x) && (v1.y == v2.y);
+    }
+
+    public static bool operator !=(Vector2Int v1, Vector2Int v2)
+    {
+        return !(v1 == v2);
+    }
+    #endregion
 }
