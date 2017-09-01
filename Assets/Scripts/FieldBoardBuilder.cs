2231b832d043aa357348a374afbe1ca1e110d09a
Furikake <ewave6300@gmail.jp>
2017-09-01 15:26:04 +0900

    建築用クラスFieldBoardBuilder作成　フィールドにくっつけて使う

---
+++ Assets/Scripts/FieldBoardBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldBoardBuilder : MonoBehaviour {
+
+    public Facility selectedFacility { get; set; }   //現在設置選択しているファシリティ
+
+	// Use this for initialization
+	void Start () {
+        //無を選択
+        selectedFacility = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
