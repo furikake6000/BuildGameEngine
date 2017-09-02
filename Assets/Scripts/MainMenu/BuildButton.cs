a87b2b8b62bbf3a6d7fffa9739e28af506afc141
Furikake <ewave63@keio.jp>
2017-09-03 02:37:58 +0900

    Facility一覧を自動取得するボタンを実装 （まだ開閉動作・おした時の動作は無し）

---
+++ Assets/Scripts/MainMenu/BuildButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildButton : MonoBehaviour {
+
+    private Facility facility;
+    private Image facilityImage;
+    private Text facilityText;
+
+    public Facility Facility
+    {
+        get
+        {
+            return facility;
+        }
+
+        set
+        {
+            facility = value;
+
+            //もしロードされてなければロードする
+            if (facilityImage == null) facilityImage = this.GetComponentsInChildren<Image>()[1];
+            if (facilityText == null) facilityText = this.GetComponentInChildren<Text>();
+
+            if (facility == null)
+            {
+                facilityImage = null;
+                facilityText.text = "";
+            }
+            else
+            {
+                facilityImage.sprite = facility.gameObject.GetComponent<SpriteRenderer>().sprite;
+                facilityText.text = facility.FacilityName;
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPressed()
+    {
+        //自分のボタンに該当する施設を選択する
+        FieldBoardBuilder.SelectedFacility = facility;
+    }
+}
---
+++ Assets/Scripts/MainMenu/BuildPanelManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPanelManager : MonoBehaviour {
+    
+    private Animator panelAnimator;
+    private bool isOpen;
+    private Facility[] facilityPrefabs;
+    private BuildButton[] facilityButtons;
+    private RectTransform[] facilityButtonTransform;
+
+    [SerializeField]
+    private Animator attachedButtonAnimator;
+    [SerializeField]
+    private GameObject facilityButtonPrefab;    //ボタンのプレハブ
+
+    private void Start()
+    {
+        panelAnimator = this.GetComponent<Animator>();
+
+        //登録されているFaciitiesを一括取得
+        //同時にボタンを作成
+        facilityPrefabs = Resources.LoadAll<Facility>("Facilities");
+        facilityButtons = new BuildButton[facilityPrefabs.Length];
+        facilityButtonTransform = new RectTransform[facilityPrefabs.Length];
+        for(var i = 0; i < facilityPrefabs.Length; i++)
+        {
+            facilityButtons[i] = GameObject.Instantiate(facilityButtonPrefab, this.transform).GetComponent<BuildButton>();
+            //施設セット
+            facilityButtons[i].Facility = facilityPrefabs[i];
+            //座標あわせ
+            facilityButtonTransform[i] = facilityButtons[i].gameObject.GetComponent<RectTransform>();
+            facilityButtonTransform[i].anchoredPosition = new Vector3(-350f + 200f * i, -114f, 0f);
+        }
+    }
+
+    private void Update()
+    {
+        if (!isOpen && attachedButtonAnimator.GetBool("isPressed"))
+        {
+            OpenPanel();
+        }
+
+        if (isOpen && !attachedButtonAnimator.GetBool("isPressed"))
+        {
+            ClosePanel();
+        }
+    }
+
+    public void OpenPanel()
+    {
+        isOpen = true;
+        panelAnimator.SetBool("open", true);
+    }
+
+    public void ClosePanel()
+    {
+        isOpen = false;
+        panelAnimator.SetBool("open", false);
+    }
+}
