68c3fb26315c06d5ba2d297603db991f3a3c4791
Furikake <ewave63@keio.jp>
2017-09-14 02:45:53 +0900

    背景&背景遷移グラデーション追加

--- Assets/BuildingGameEngine/Scripts/FieldBoardTimeManager.cs
+++ Assets/BuildingGameEngine/Scripts/FieldBoardTimeManager.cs
@@ -14,6 +14,16 @@ public class FieldBoardTimeManager : MonoBehaviour {
         hourEventActivate, minuteEventActivate, secondEventActivate;
     [SerializeField]
     private VirtualClockSpan fieldTimeUpdateAddSpan;  //フィールド時間を更新するたびに加算される時間
+
+    [SerializeField]
+    private Image backImage;
+    [SerializeField]
+    private Image backImageFade;    //背景とフェード用重ねイメージ
+    [SerializeField]
+    private int morningHour = 6, dayHour = 8, eveningHour = 18, nightHour = 20;    //朝昼夕夜が開始する時間
+    [SerializeField]
+    private Sprite morningBack, dayBack, eveningBack, nightBack;    //それぞれの背景
+
     [SerializeField]
     private bool fieldTimeEnabled;  //フィールド時間が動いているか止まっているか
     
@@ -29,6 +39,7 @@ public class FieldBoardTimeManager : MonoBehaviour {
         //時間は2000年1月1日にリセット
         fieldTime = startTime;
         RefreshClockView();
+        RefreshBackImage();
     }
 
     // Use this for initialization
@@ -53,7 +64,7 @@ public class FieldBoardTimeManager : MonoBehaviour {
             
             //時間の加算
             fieldTime.Add(fieldTimeUpdateAddSpan);
-            RefreshClockView();
+            RefreshBackImage();
 
             //毎年行われる処理
 
@@ -72,11 +83,71 @@ public class FieldBoardTimeManager : MonoBehaviour {
     }
     #endregion
 
-    private void RefreshClockView()
+    public void RefreshClockView()
     {
         //表示変更
         dateLabel.text = fieldTime.month + "月" + fieldTime.day + "日";
         timeLabel.text = fieldTime.hour + ":" + fieldTime.minute.ToString("D2");
+    }
+
+    public void RefreshBackImage()
+    {
+        if(fieldTime.hour < morningHour || fieldTime.hour >= nightHour)
+        {
+            //よる
+            backImage.sprite = nightBack;
+            if(fieldTime.hour == morningHour - 1)
+            {
+                backImageFade.sprite = morningBack;
+                backImageFade.color = new Color(1f, 1f, 1f, (float)fieldTime.minute / 60f);
+            }
+            else
+            {
+                backImageFade.color = new Color(1f, 1f, 1f, 0f);
+            }
+        }
+        else if(fieldTime.hour < dayHour)
+        {
+            //あさ
+            backImage.sprite = morningBack;
+            if (fieldTime.hour == dayHour - 1)
+            {
+                backImageFade.sprite = dayBack;
+                backImageFade.color = new Color(1f, 1f, 1f, (float)fieldTime.minute / 60f);
+            }
+            else
+            {
+                backImageFade.color = new Color(1f, 1f, 1f, 0f);
+            }
+        }
+        else if(fieldTime.hour < eveningHour)
+        {
+            //ひる
+            backImage.sprite = dayBack;
+            if (fieldTime.hour == eveningHour - 1)
+            {
+                backImageFade.sprite = eveningBack;
+                backImageFade.color = new Color(1f, 1f, 1f, (float)fieldTime.minute / 60f);
+            }
+            else
+            {
+                backImageFade.color = new Color(1f, 1f, 1f, 0f);
+            }
+        }
+        else
+        {
+            //ゆうがた
+            backImage.sprite = eveningBack;
+            if (fieldTime.hour == nightHour - 1)
+            {
+                backImageFade.sprite = nightBack;
+                backImageFade.color = new Color(1f, 1f, 1f, (float)fieldTime.minute / 60f);
+            }
+            else
+            {
+                backImageFade.color = new Color(1f, 1f, 1f, 0f);
+            }
+        }
     }
 
     public void ToggleClockEnabled()
