975ffecbef2ccad567a7ccc08bf8e04a7f2e291f
Furikake <ewave63@keio.jp>
2017-09-15 05:00:31 +0900

    Creatureクラスの作成

---
+++ Assets/Scripts/Creatures/Creature.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour {
+
+    [SerializeField]
+    private string creatureName;
+
+    public string CreatureName
+    {
+        get
+        {
+            return creatureName;
+        }
+
+        set
+        {
+            creatureName = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
