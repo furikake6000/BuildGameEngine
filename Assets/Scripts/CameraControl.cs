a02768f22e44b16c648d5ecd636170ffc540e8f2
Furikake <ewave63@keio.jp>
2017-09-15 16:18:34 +0900

    カメラは建築モード時には起動しないように

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -21,46 +21,47 @@ public class CameraControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-        if(Input.touchCount == 1)
+        //ビルダーの選択がない時だけ実行
+        if(FieldBoardBuilder.SelectedFacility == null)
         {
-            //スライド処理
-            Touch touchPoint = Input.GetTouch(0);
-
-            if(touchPoint.phase != TouchPhase.Began)
+            if (Input.touchCount == 1)
             {
-                //カメラの座標ベクトルを作成
-                Vector3 deltaVec = new Vector3(touchPoint.deltaPosition.x, touchPoint.deltaPosition.y, 0f);
-                deltaVec = -deltaVec * moveRate / Camera.main.orthographicSize;
+                //スライド処理
+                Touch touchPoint = Input.GetTouch(0);
 
-                //カメラ移動
-                Camera.main.transform.position += deltaVec;
+                if (touchPoint.phase != TouchPhase.Began)
+                {
+                    //カメラの座標ベクトルを作成
+                    Vector3 deltaVec = new Vector3(touchPoint.deltaPosition.x, touchPoint.deltaPosition.y, 0f);
+                    deltaVec = -deltaVec * moveRate / Camera.main.orthographicSize;
+
+                    //カメラ移動
+                    Camera.main.transform.position += deltaVec;
+                }
+
+                pastTouch[0] = touchPoint;
             }
-
-            pastTouch[0] = touchPoint;
-        }
-        else if(Input.touchCount >= 2)
-        {
-            //ピンチイン・ピンチアウト処理
-            Touch t1 = Input.GetTouch(0);
-            Touch t2 = Input.GetTouch(1);
-
-            if(t1.phase != TouchPhase.Began && t2.phase != TouchPhase.Began)
+            else if (Input.touchCount >= 2)
             {
-                //タッチ距離の差分を取得
-                float deltaDist = Vector2.Distance(pastTouch[0].position, pastTouch[1].position)
-                                    - Vector2.Distance(t1.position, t2.position);
-                //拡大
-                Camera.main.orthographicSize += deltaDist * zoomRate;
-                //最大最小値と照らし合わせる
-                if (Camera.main.orthographicSize > camSizeMax) Camera.main.orthographicSize = camSizeMax;
-                if (Camera.main.orthographicSize < camSizeMin) Camera.main.orthographicSize = camSizeMin;
+                //ピンチイン・ピンチアウト処理
+                Touch t1 = Input.GetTouch(0);
+                Touch t2 = Input.GetTouch(1);
+
+                if (t1.phase != TouchPhase.Began && t2.phase != TouchPhase.Began)
+                {
+                    //タッチ距離の差分を取得
+                    float deltaDist = Vector2.Distance(pastTouch[0].position, pastTouch[1].position)
+                                        - Vector2.Distance(t1.position, t2.position);
+                    //拡大
+                    Camera.main.orthographicSize += deltaDist * zoomRate;
+                    //最大最小値と照らし合わせる
+                    if (Camera.main.orthographicSize > camSizeMax) Camera.main.orthographicSize = camSizeMax;
+                    if (Camera.main.orthographicSize < camSizeMin) Camera.main.orthographicSize = camSizeMin;
+                }
+
+                pastTouch[0] = t1;
+                pastTouch[1] = t2;
             }
-
-            pastTouch[0] = t1;
-            pastTouch[1] = t2;
         }
-
-
-
     }
 }
