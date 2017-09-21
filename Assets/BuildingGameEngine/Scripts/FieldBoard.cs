e84af02b28ca708ae2f73bbac84e9c3388015592
Furikake <ewave63@keio.jp>
2017-09-22 00:16:03 +0900

    一部変数名をわかりやすく変更

--- Assets/BuildingGameEngine/Scripts/FieldBoard.cs
+++ Assets/BuildingGameEngine/Scripts/FieldBoard.cs
@@ -10,11 +10,36 @@ using UnityEngine.UI;
 public class FieldBoard : MonoBehaviour {
 
     #region Inspector表示変数
-    [Header("Graphic data")]
     [SerializeField]
-    private int xTileSize, yTileSize;
+    private int mapWidth, mapHeight;
+    public int MapWidth
+    {
+        get
+        {
+            return mapWidth;
+        }
+
+        set
+        {
+            mapWidth = value;
+            RefreshMeshes();
+        }
+    }
+    public int MapHeight
+    {
+        get
+        {
+            return mapHeight;
+        }
+
+        set
+        {
+            mapHeight = value;
+            RefreshMeshes();
+        }
+    }
     [SerializeField]
-    private float widthOfTile, aspectRatioOfTile;
+    private float sizeOfTile, aspectRatioOfTile;
     [SerializeField]
     private float edgeWeight;
     [SerializeField]
@@ -86,6 +111,7 @@ public class FieldBoard : MonoBehaviour {
             visitors = value;
         }
     }
+
     private List<Visitor> visitors; //観客
 
 
@@ -112,7 +138,7 @@ public class FieldBoard : MonoBehaviour {
         visitors = new List<Visitor>();
 
         //マス高さ算出
-        heightOfTile = widthOfTile * aspectRatioOfTile;
+        heightOfTile = sizeOfTile * aspectRatioOfTile;
 
         //資金設定
         Money = defaultMoney;
@@ -156,13 +182,13 @@ public class FieldBoard : MonoBehaviour {
     public Vector3 MapPosToWorldPos(Vector2 mapPos)
     {
         //マップの中心座標との相対位置を求める
-        Vector2 mapPosRelativeFromCenter = new Vector2(mapPos.x - (float)(xTileSize - 1) / 2, mapPos.y - (float)(yTileSize - 1) / 2);
+        Vector2 mapPosRelativeFromCenter = new Vector2(mapPos.x - (float)(MapWidth - 1) / 2, mapPos.y - (float)(MapHeight - 1) / 2);
 
         //WorldPos.x = ( mapPosRFC.x - mapPosRFC.y ) / -2f
         //WorldPos.y = ( mapPosRFC.x + mapPosRFC.y ) / -2f
         //WorldPos.z = ( mapPosRFC.x + mapPosRFC.y ) / -100f(1段下に降りるごとに0.01fだけ変化)
         Vector3 worldPos = new Vector3(
-            (mapPosRelativeFromCenter.x - mapPosRelativeFromCenter.y) / -2f * widthOfTile,
+            (mapPosRelativeFromCenter.x - mapPosRelativeFromCenter.y) / -2f * sizeOfTile,
             (mapPosRelativeFromCenter.x + mapPosRelativeFromCenter.y) / -2f * heightOfTile,
             (mapPosRelativeFromCenter.x + mapPosRelativeFromCenter.y) / -100f);
 
@@ -175,7 +201,7 @@ public class FieldBoard : MonoBehaviour {
         Vector3 worldPosVecFromZero = worldPos - MapPosToWorldPos(Vector2.zero);
 
         //X、Yそれぞれの軸においてworldPosVecFromZeroの要素が何マス分にあたるか計算
-        float xFromZero = worldPosVecFromZero.x / widthOfTile;
+        float xFromZero = worldPosVecFromZero.x / sizeOfTile;
         float yFromZero = worldPosVecFromZero.y / heightOfTile;
 
         //二次元ベクトルに変換
@@ -193,9 +219,9 @@ public class FieldBoard : MonoBehaviour {
         var fieldMesh = new Mesh();
         //頂点配列を作成（ xマスの数+1 * yマスの数+1 個）
         var points = new List<Vector3>();
-        for (var y = 0; y < yTileSize + 1; y++)
+        for (var y = 0; y < MapHeight + 1; y++)
         {
-            for (var x = 0; x < xTileSize + 1; x++)
+            for (var x = 0; x < MapWidth + 1; x++)
             {
                 //各タイルの頂点をインプット（中心点ではなく頂点なので0.5fずらしている）
                 points.Add(MapPosToWorldPos(new Vector2((float)x - 0.5f, (float)y - 0.5f)));
@@ -204,19 +230,19 @@ public class FieldBoard : MonoBehaviour {
         fieldMesh.vertices = points.ToArray();
         //四角形メッシュ配列を作成（三角形が xマスの数 * yマスの数 個）
         var triangles = new List<int>();
-        for (var x = 0; x < xTileSize; x++)
+        for (var x = 0; x < MapWidth; x++)
         {
-            for (var y = 0; y < yTileSize; y++)
+            for (var y = 0; y < MapHeight; y++)
             {
                 //三角形を張る（表から見て時計回りの順番に！）
                 //上半分の三角形
-                triangles.Add(x + y * (xTileSize + 1));
-                triangles.Add((x + 1) + y * (xTileSize + 1));
-                triangles.Add(x + (y + 1) * (xTileSize + 1));
+                triangles.Add(x + y * (MapWidth + 1));
+                triangles.Add((x + 1) + y * (MapWidth + 1));
+                triangles.Add(x + (y + 1) * (MapWidth + 1));
                 //下半分の三角形
-                triangles.Add((x + 1) + y * (xTileSize + 1));
-                triangles.Add((x + 1) + (y + 1) * (xTileSize + 1));
-                triangles.Add(x + (y + 1) * (xTileSize + 1));
+                triangles.Add((x + 1) + y * (MapWidth + 1));
+                triangles.Add((x + 1) + (y + 1) * (MapWidth + 1));
+                triangles.Add(x + (y + 1) * (MapWidth + 1));
             }
         }
         fieldMesh.triangles = triangles.ToArray();
@@ -231,30 +257,30 @@ public class FieldBoard : MonoBehaviour {
         var gridMesh = new Mesh();
         //頂点配列の作成（グリッドなので隅の点だけでよい）
         var gridPoints = new List<Vector3>();
-        for (var x = 0; x < xTileSize + 1; x++)
+        for (var x = 0; x < MapWidth + 1; x++)
         {
             //上端2点
             gridPoints.Add(MapPosToWorldPos(new Vector2((float)x - 0.5f - edgeWeight / 2, -0.5f - edgeWeight / 2)));
             gridPoints.Add(MapPosToWorldPos(new Vector2((float)x - 0.5f + edgeWeight / 2, -0.5f - edgeWeight / 2)));
 
             //下端2点
-            gridPoints.Add(MapPosToWorldPos(new Vector2((float)x - 0.5f - edgeWeight / 2, (float)yTileSize - 0.5f + edgeWeight / 2)));
-            gridPoints.Add(MapPosToWorldPos(new Vector2((float)x - 0.5f + edgeWeight / 2, (float)yTileSize - 0.5f + edgeWeight / 2)));
+            gridPoints.Add(MapPosToWorldPos(new Vector2((float)x - 0.5f - edgeWeight / 2, (float)MapHeight - 0.5f + edgeWeight / 2)));
+            gridPoints.Add(MapPosToWorldPos(new Vector2((float)x - 0.5f + edgeWeight / 2, (float)MapHeight - 0.5f + edgeWeight / 2)));
         }
-        for (var y = 0; y < yTileSize + 1; y++)
+        for (var y = 0; y < MapHeight + 1; y++)
         {
             //左端2点
             gridPoints.Add(MapPosToWorldPos(new Vector2(-0.5f - edgeWeight / 2, (float)y - 0.5f - edgeWeight / 2)));
             gridPoints.Add(MapPosToWorldPos(new Vector2(-0.5f - edgeWeight / 2, (float)y - 0.5f + edgeWeight / 2)));
 
             //右端2点
-            gridPoints.Add(MapPosToWorldPos(new Vector2((float)xTileSize - 0.5f - edgeWeight / 2, (float)y - 0.5f - edgeWeight / 2)));
-            gridPoints.Add(MapPosToWorldPos(new Vector2((float)xTileSize - 0.5f - edgeWeight / 2, (float)y - 0.5f + edgeWeight / 2)));
+            gridPoints.Add(MapPosToWorldPos(new Vector2((float)MapWidth - 0.5f - edgeWeight / 2, (float)y - 0.5f - edgeWeight / 2)));
+            gridPoints.Add(MapPosToWorldPos(new Vector2((float)MapWidth - 0.5f - edgeWeight / 2, (float)y - 0.5f + edgeWeight / 2)));
         }
         gridMesh.vertices = gridPoints.ToArray();
         //四角形メッシュ配列を作成
         var gridTriangles = new List<int>();
-        for (var x = 0; x < xTileSize + 1; x++)
+        for (var x = 0; x < MapWidth + 1; x++)
         {
             //三角形ひとつめ
             gridTriangles.Add(x * 4);
@@ -265,16 +291,16 @@ public class FieldBoard : MonoBehaviour {
             gridTriangles.Add(x * 4 + 2);
             gridTriangles.Add(x * 4 + 3);
         }
-        for (var y = 0; y < yTileSize + 1; y++)
+        for (var y = 0; y < MapHeight + 1; y++)
         {
             //三角形ひとつめ
-            gridTriangles.Add((xTileSize + 1) * 4 + y * 4);
-            gridTriangles.Add((xTileSize + 1) * 4 + y * 4 + 2);
-            gridTriangles.Add((xTileSize + 1) * 4 + y * 4 + 1);
+            gridTriangles.Add((MapWidth + 1) * 4 + y * 4);
+            gridTriangles.Add((MapWidth + 1) * 4 + y * 4 + 2);
+            gridTriangles.Add((MapWidth + 1) * 4 + y * 4 + 1);
             //三角形ふたつめ
-            gridTriangles.Add((xTileSize + 1) * 4 + y * 4 + 1);
-            gridTriangles.Add((xTileSize + 1) * 4 + y * 4 + 2);
-            gridTriangles.Add((xTileSize + 1) * 4 + y * 4 + 3);
+            gridTriangles.Add((MapWidth + 1) * 4 + y * 4 + 1);
+            gridTriangles.Add((MapWidth + 1) * 4 + y * 4 + 2);
+            gridTriangles.Add((MapWidth + 1) * 4 + y * 4 + 3);
         }
         gridMesh.triangles = gridTriangles.ToArray();
         //法線の再計算
@@ -295,8 +321,8 @@ public class FieldBoard : MonoBehaviour {
     {
         //範囲外チェック
         if (
-            location.x + facilityPrefab.Size.x > xTileSize || 
-            location.y + facilityPrefab.Size.y > yTileSize ||
+            location.x + facilityPrefab.Size.x > MapWidth || 
+            location.y + facilityPrefab.Size.y > MapHeight ||
             location.x < 0 || 
             location.y < 0)
         {
@@ -331,7 +357,7 @@ public class FieldBoard : MonoBehaviour {
         Vector3 newFacilityPos = MapPosToWorldPos((Vector2)location + (Vector2)(facilityPrefab.Size - new Vector2Int(1, 1)) / 2);
         //縦向きのズレを算出
         SpriteRenderer facilityPrefabRenderer = facilityPrefab.gameObject.GetComponent<SpriteRenderer>();
-        float yGap = (facilityPrefabRenderer.bounds.size.y - facilityPrefabRenderer.bounds.size.x * aspectRatioOfTile) / 2 * widthOfTile;
+        float yGap = (facilityPrefabRenderer.bounds.size.y - facilityPrefabRenderer.bounds.size.x * aspectRatioOfTile) / 2 * sizeOfTile;
         //ズレ適用
         newFacilityPos.y += yGap;
 
@@ -357,8 +383,8 @@ public class FieldBoard : MonoBehaviour {
     {
         //範囲外チェック
         if (
-            location.x > xTileSize ||
-            location.y > yTileSize ||
+            location.x > MapWidth ||
+            location.y > MapHeight ||
             location.x < 0 ||
             location.y < 0)
         {
