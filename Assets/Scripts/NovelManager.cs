dd09fcafabbc89d76db4268aff79a2fd8c90c720
Furikake <ewave63@keio.jp>
2017-09-26 13:54:27 +0900

    ノベルパート、テキストを時間に応じてちょっとずつ表示していくように変更（速度調整も可能）

--- Assets/Scripts/NovelManager.cs
+++ Assets/Scripts/NovelManager.cs
@@ -12,6 +12,10 @@ public class NovelManager : MonoBehaviour {
     Image faceImage;
     [SerializeField]
     GameObject novelParent;  //UIセットの親オブジェクト（表示・非表示に使用）
+    [SerializeField]
+    float oneCharDispTime = 0.03f;  //一文字が表示されるのにかかる秒数
+    [SerializeField]
+    float textUpdateMarginTime = 0f; //連続ページ送りにかかるマージンタイム
 
     [SerializeField]
     Sprite[] faces; //表情差分
@@ -19,7 +23,8 @@ public class NovelManager : MonoBehaviour {
     private static Queue<int> faceID = new Queue<int>();
     private static Queue<string> message = new Queue<string>();
 
-    private float timeSinceDisplayStart;    //現在の文字列表示を開始した時間
+    private string displayingText;  //現在表示しているテキスト
+    private float timeSinceDisplayStart;    //現在の文字列表示を開始してからの時間
 
     private bool enable;
     public bool Enable
@@ -56,17 +61,38 @@ public class NovelManager : MonoBehaviour {
 
 		if(Enable)
         {
-            if((Input.touchCount >= 1 && Input.GetTouch(Input.touchCount - 1).phase == TouchPhase.Ended)
+            //経過時間プラス処理
+            timeSinceDisplayStart += Time.deltaTime;
+
+            //時間に応じて文字列を表示(経過時間/1文字表示時間 文字表示)
+            messageWindow.text = displayingText.Substring(0, Math.Min((int)(timeSinceDisplayStart / oneCharDispTime), displayingText.Length));
+
+            //タップされた時の処理
+            if ((Input.touchCount >= 1 && Input.GetTouch(Input.touchCount - 1).phase == TouchPhase.Began)
                 || Input.GetKeyDown(KeyCode.Return))
             {
-                //ページ送り
-                if (!TextUpdate())
+                //時間で分岐
+                if (timeSinceDisplayStart < oneCharDispTime * displayingText.Length)
                 {
-                    //メッセージ終了
-                    novelParent.SetActive(false);
-                    Enable = false;
-                    Tutorial1.FinishTutorial();
+                    //まだ完全に表示していない場合
+
+                    //最後まで表示する
+                    timeSinceDisplayStart = oneCharDispTime * displayingText.Length;
                 }
+                else if(timeSinceDisplayStart > oneCharDispTime * displayingText.Length + textUpdateMarginTime)
+                {
+                    //完全に表示しきって、連続タップ猶予時間も過ぎていた場合
+                    
+                    //ページ送り
+                    if (TextUpdate())
+                    {
+                        //メッセージ最後まで表示しきったら終了
+                        novelParent.SetActive(false);
+                        Enable = false;
+                        Tutorial1.FinishTutorial();
+                    }
+                }
+                
             }
         }
 	}
@@ -77,11 +103,17 @@ public class NovelManager : MonoBehaviour {
         faceID.Enqueue(inFaceID);
     }
 
+    /// <summary>
+    /// ページ送り
+    /// </summary>
+    /// <returns></returns>
     bool TextUpdate()
     {
         if(message.Count >= 1)
         {
-            messageWindow.text = message.Dequeue();
+            //テキストの更新
+            displayingText = message.Dequeue();
+            //顔グラの変更処理
             int newFaceID = faceID.Dequeue();
             if (newFaceID < 0 || newFaceID > faces.Length - 1)
             {
@@ -89,13 +121,16 @@ public class NovelManager : MonoBehaviour {
             }
             faceImage.sprite = faces[newFaceID];
 
+            //経過時間を0にリセット
+            timeSinceDisplayStart = 0f;
+
             //まだある
-            return true;
+            return false;
         }
         else
         {
             //もうない
-            return false;
+            return true;
         }
         
     }
