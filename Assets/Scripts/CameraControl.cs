0d4a24f11acaddb215c4393ecab77bd17e167430
Furikake <ewave63@keio.jp>
2017-10-06 14:44:55 +0900

    マウスホイールによる拡大縮小を実装

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -6,6 +6,8 @@ using UnityEngine.EventSystems;
 
 public class CameraControl : MonoBehaviour, IDragHandler {
 
+    const float MouseWheelFix = 5.0f;
+
     [SerializeField]
     private float camSizeMin = 2f, camSizeMax = 5f; //拡大・縮小の極限値
     [SerializeField]
@@ -41,57 +43,12 @@ public class CameraControl : MonoBehaviour, IDragHandler {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		//マウスホイール回転による拡大縮小
         //ビルダーの選択がない時だけ実行
         if(builder.SelectedFacility == null)
         {
-            //if (Input.touchCount == 1)
-            //{
-            //    //スライド処理
-            //    Touch touchPoint = Input.GetTouch(0);
-
-            //    if (touchPoint.phase != TouchPhase.Ended)
-            //    {
-            //        //カメラの座標差分ベクトルを作成
-            //        Vector3 deltaVec = new Vector3(touchPoint.deltaPosition.x, touchPoint.deltaPosition.y, 0f);
-            //        deltaVec = -deltaVec * moveRate / pixelPerCm;
-
-            //        //カメラ移動
-            //        Vector3 newCamPos = Camera.main.transform.position + deltaVec;
-
-            //        //四隅移動制限処理
-            //        if (newCamPos.y > topLimit) newCamPos.y = topLimit;
-            //        if (newCamPos.y < bottomLimit) newCamPos.y = bottomLimit;
-            //        if (newCamPos.x < leftLimit) newCamPos.x = leftLimit;
-            //        if (newCamPos.x > rightLimit) newCamPos.x = rightLimit;
-
-            //        //座標に適用
-            //        Camera.main.transform.position = newCamPos;
-            //    }
-
-            //    pastTouch[0] = touchPoint;
-            //}
-            //else if (Input.touchCount >= 2)
-            //{
-            //    //ピンチイン・ピンチアウト処理
-            //    Touch t1 = Input.GetTouch(0);
-            //    Touch t2 = Input.GetTouch(1);
-
-            //    if (t1.phase != TouchPhase.Began && t2.phase != TouchPhase.Began)
-            //    {
-            //        //タッチ距離の差分を取得
-            //        float deltaDist = Vector2.Distance(pastTouch[0].position, pastTouch[1].position)
-            //                            - Vector2.Distance(t1.position, t2.position);
-            //        //拡大
-            //        Camera.main.orthographicSize += deltaDist * zoomRate / pixelPerCm;
-            //        //最大最小値と照らし合わせる
-            //        if (Camera.main.orthographicSize > camSizeMax) Camera.main.orthographicSize = camSizeMax;
-            //        if (Camera.main.orthographicSize < camSizeMin) Camera.main.orthographicSize = camSizeMin;
-            //    }
-
-            //    pastTouch[0] = t1;
-            //    pastTouch[1] = t2;
-            //}
+            Zoom( - Input.GetAxis("Mouse ScrollWheel") * MouseWheelFix * zoomRate);
         }
     }
     
@@ -157,28 +114,37 @@ public class CameraControl : MonoBehaviour, IDragHandler {
 
                     pastTouch[0] = touchPoint;
                 }
-                else if (Input.touchCount >= 2)
+                else if (Input.touchCount == 2)
                 {
-                    //ピンチイン・ピンチアウト処理
                     Touch t1 = Input.GetTouch(0);
                     Touch t2 = Input.GetTouch(1);
-
-                    if (t1.phase != TouchPhase.Began && t2.phase != TouchPhase.Began)
+                    if (t2.phase != TouchPhase.Began)
                     {
-                        //タッチ距離の差分を取得
-                        float deltaDist = Vector2.Distance(pastTouch[0].position, pastTouch[1].position)
-                                            - Vector2.Distance(t1.position, t2.position);
-                        //拡大
-                        Camera.main.orthographicSize += deltaDist * zoomRate / pixelPerCm;
-                        //最大最小値と照らし合わせる
-                        if (Camera.main.orthographicSize > camSizeMax) Camera.main.orthographicSize = camSizeMax;
-                        if (Camera.main.orthographicSize < camSizeMin) Camera.main.orthographicSize = camSizeMin;
-                    }
+                        //タッチ開始時でなければ
+                        //ピンチイン・ピンチアウト処理
 
+                        if (t1.phase != TouchPhase.Began && t2.phase != TouchPhase.Began)
+                        {
+                            //タッチ距離の差分を取得
+                            float deltaDist = Vector2.Distance(pastTouch[0].position, pastTouch[1].position)
+                                                - Vector2.Distance(t1.position, t2.position);
+                            //拡大
+                            Zoom(deltaDist * zoomRate / pixelPerCm);
+                        }
+                    }
                     pastTouch[0] = t1;
                     pastTouch[1] = t2;
                 }
             }
         }
+    }
+
+    private void Zoom(float rate)
+    {
+        //拡大
+        Camera.main.orthographicSize += rate;
+        //最大最小値と照らし合わせる
+        if (Camera.main.orthographicSize > camSizeMax) Camera.main.orthographicSize = camSizeMax;
+        if (Camera.main.orthographicSize < camSizeMin) Camera.main.orthographicSize = camSizeMin;
     }
 }
