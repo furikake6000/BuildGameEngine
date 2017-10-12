a22d000e3ea434701450042ec5e9e4e9da3683f8
Furikake <ewave63@keio.jp>
2017-10-12 21:22:32 +0900

    BuildButtonのソースコードを作成

---
+++ Assets/Scripts/MainMenu/BuildButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BuildButton : MonoBehaviour, IPointerDownHandler {
+
+    Animator animator;
+    [SerializeField]
+    BuildPanelManager linkedBuildPanel; //このボタンを押すことによって開くパネル
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        animator.SetBool("isPressed", !animator.GetBool("isPressed"));
+    }
+}
