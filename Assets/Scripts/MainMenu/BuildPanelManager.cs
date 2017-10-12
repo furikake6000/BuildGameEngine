cff15645370f2b00f1ef2ec9341008eb9241722c
Furikake <ewave63@keio.jp>
2017-10-12 14:09:12 +0900

    BuildPanelManagerにおいて、UIClipを利用することによりManager側からボタンなどの座標指定をする必要がなくなりコードがシンプルに

--- Assets/Scripts/MainMenu/BuildPanelManager.cs
+++ Assets/Scripts/MainMenu/BuildPanelManager.cs
@@ -3,15 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildPanelManager : MonoBehaviour {
-
-    //オブジェクトを視認外に移動させたい時の座標（定数）
-    private static readonly Vector3 HidePosition = new Vector3(-9999f, -9999f, -9999f);
-
+    
     private Animator animator;
     private bool isOpen, isCompletelyOpen;  //それぞれ「開き始め～閉じ始め」「開き終わり～閉じ始め」にONとなる
-    private Facility[] facilityPrefabs;
-    private BuildPanelButton[] facilityButtons;
-    private RectTransform[] facilityButtonTransform;
 
     [SerializeField]
     private Animator attachedButtonAnimator;
@@ -25,25 +19,27 @@ public class BuildPanelManager : MonoBehaviour {
         animator = this.GetComponent<Animator>();
         builder = GameObject.FindGameObjectWithTag("FieldBoard").GetComponent<FieldBoardBuilder>();
 
+        RefreshFacilityButtons();
+    }
+
+    private void RefreshFacilityButtons()
+    {
         //登録されているFaciitiesを一括取得
         //同時にボタンを作成
-        List<Facility> allFacilityPrefabs = new List<Facility>(Resources.LoadAll<Facility>("Facilities"));
-        for(var i = allFacilityPrefabs.Count - 1; i >= 0; i--)
+        List<Facility> facilityPrefabs = new List<Facility>(Resources.LoadAll<Facility>("Facilities"));
+        for (var i = facilityPrefabs.Count - 1; i >= 0; i--)
         {
-            if (!allFacilityPrefabs[i].Buildable) allFacilityPrefabs.RemoveAt(i);
+            //Buildできないものは取り除く
+            if (!facilityPrefabs[i].Buildable) facilityPrefabs.RemoveAt(i);
         }
-        facilityPrefabs = allFacilityPrefabs.ToArray();
-        facilityButtons = new BuildPanelButton[facilityPrefabs.Length];
-        facilityButtonTransform = new RectTransform[facilityPrefabs.Length];
-        for(var i = 0; i < facilityPrefabs.Length; i++)
+        for (var i = 0; i < facilityPrefabs.Count; i++)
         {
-            facilityButtons[i] = GameObject.Instantiate(facilityButtonPrefab, this.transform).GetComponent<BuildPanelButton>();
+            BuildPanelButton newFacilityButton;
+            newFacilityButton = GameObject.Instantiate(facilityButtonPrefab, this.transform).GetComponent<BuildPanelButton>();
             //施設セット
-            facilityButtons[i].Facility = facilityPrefabs[i];
+            newFacilityButton.Facility = facilityPrefabs[i];
             //座標あわせ
-            facilityButtonTransform[i] = facilityButtons[i].gameObject.GetComponent<RectTransform>();
-            //facilityButtonTransform[i].anchoredPosition = new Vector3(-350f + 200f * i, -114f, 0f);
-            facilityButtonTransform[i].anchoredPosition = HidePosition;
+            newFacilityButton.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-350f + 200f * i, 215f, 0f);
         }
     }
 
@@ -55,26 +51,11 @@ public class BuildPanelManager : MonoBehaviour {
             OpenPanel();
         }
 
-        if(!isCompletelyOpen && attachedButtonAnimator.GetBool("isPressed") && attachedButtonAnimator.GetBool("animationFinished"))
-        {
-            //パネル完全に開いた処理
-            isCompletelyOpen = true;
-            for (var i = 0; i < facilityButtonTransform.Length; i++)
-            {
-                //座標あわせ
-                facilityButtonTransform[i].anchoredPosition = new Vector3(-350f + 200f * i, -114f, 0f);
-            }
-        }
-
         if (isOpen && !attachedButtonAnimator.GetBool("isPressed"))
         {
+            //パネル閉じ処理
             ClosePanel();
             isCompletelyOpen = false;
-            for (var i = 0; i < facilityButtonTransform.Length; i++)
-            {
-                //座標あわせ(見えない所に)
-                facilityButtonTransform[i].anchoredPosition = HidePosition;
-            }
         }
     }
 
