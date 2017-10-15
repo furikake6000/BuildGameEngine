d141c3ee70bff4db1baabffa47f3dc03fa981b48
Furikake <ewave63@keio.jp>
2017-10-15 22:08:29 +0900

    時間停止ボタンを作成

---
+++ Assets/Scripts/MainMenu/ClockButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockButton : ToggleButton {
+    
+    protected override void Action(bool isPressed)
+    {
+        FieldTimeManager.ToggleClockEnabledStatic();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        animator.SetBool("isPressed", FieldTimeManager.TimeEnabled);
+    }
+
+}
