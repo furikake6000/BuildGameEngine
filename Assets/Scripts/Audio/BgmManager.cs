80fe76f9e1f6ef125eaf03b5a984ca47a3f169d9
Furikake <ewave63@keio.jp>
2017-10-18 18:00:10 +0900

    AudioManagerは（AudioMixerを使うため）廃止、代わりにBGM流す用のBGMSourceとMasterListenerを使う

---
+++ Assets/Scripts/Audio/BgmManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgmManager : MonoBehaviour {
+
+    static AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void ChangeBgm(AudioClip source)
+    {
+        audioSource.clip = source;
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/Audio/MasterListener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterListener : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Camera.main != null)
+        {
+            //カメラが存在すれば、その座標に自らを移動する
+            transform.position = Camera.main.transform.position;
+
+            //※MasterListenerは独立シーンAudioに存在するため子オブジェクトにして追従することは出来ない
+        }
+	}
+}
