60fadac63ca987d506062b6cd9664baa11121cb1
Furikake <ewave63@keio.jp>
2017-10-19 18:20:31 +0900

    EventからシーンにアクセスするSceneManagerAccessorを追加

---
+++ Assets/Scripts/SceneManagerAccessor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerAccessor : MonoBehaviour {
+
+    //EventTriggerとかからシーン追加・破棄するためだけのコンポーネント
+
+    public void AddScene(string sceneName)
+    {
+        MultiSceneManager.AddScene(sceneName);
+    }
+    public void RemoveScene(string sceneName)
+    {
+        MultiSceneManager.RemoveScene(sceneName);
+    }
+    public void QuitGame()
+    {
+        MultiSceneManager.QuitGame();
+    }
+}
