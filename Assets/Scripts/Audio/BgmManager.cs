6cf1ae036450b76559a394b6fbbe9f33538d15aa
Furikake <ewave63@keio.jp>
2017-10-19 13:19:04 +0900

    BGMManagerのaudioSource読み込み前に呼び出される恐れあるためAssertion追加

--- Assets/Scripts/Audio/BgmManager.cs
+++ Assets/Scripts/Audio/BgmManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class BgmManager : MonoBehaviour {
 
@@ -11,9 +12,26 @@ public class BgmManager : MonoBehaviour {
         audioSource = GetComponent<AudioSource>();
     }
 
+    /// <summary>
+    /// BGMを変更する
+    /// （Audioシーンを作成した直後に呼ばない！1フレーム待つこと！）
+    /// </summary>
+    /// <param name="source">変更先BGM</param>
     public static void ChangeBgm(AudioClip source)
     {
+        //audioSourceが未だロードされてなければ早すぎる呼び出し、不適切
+        Assert.IsNotNull(audioSource, "Don't call ChangeBgm() before audioscene loaded!");
         audioSource.clip = source;
         audioSource.Play();
+    }
+    /// <summary>
+    /// BGMを停止する
+    /// （Audioシーンを作成した直後に呼ばない！1フレーム待つこと！）
+    /// </summary>
+    public static void StopBgm()
+    {
+        //audioSourceが未だロードされてなければ早すぎる呼び出し、不適切
+        Assert.IsNotNull(audioSource, "Don't call StopBgm() before audioscene loaded!");
+        audioSource.Stop();
     }
 }
